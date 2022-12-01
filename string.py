48579bbed193b1b8c05c7e798f8483cd17ce02cc
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-01 19:43:06 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,21 @@
+'''
+DATE:01TH NOV 2022
+DAY: THURSDAY
+TOPIC: STRINGS
+AUTHOR: RAMA BHARGAVI
+'''
+print(dir(str))
+a="rama"
+print(type(a))#str
+print(a.capitalize())#Rama
+print(a.center(20))#     rama
+print(a.center(20,'@'))#@@@@rama@@@@
+print(a.casefold())#rama
+a="rama"
+k=a.encode()
+print(k)#b'rama'
+print(k.decode())#rama
+print(a.upper())#RAMA
+print(a.lower())#rama
+print(a.isupper())#False
+print(a.islower())#True
